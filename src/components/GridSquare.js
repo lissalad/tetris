c67519976f2eb7227f8a9eab3d16054975984f6c
lissalad <elissalaymon@gmail.com>
2022-09-05 14:10:11 -0700

    added initial next block

--- src/components/GridSquare.js
+++ src/components/GridSquare.js
@@ -1,4 +1,4 @@
-export default function GridsSquare(props) {
+export default function GridSquare(props) {
   const classes = `grid-square color-${props.color}`;
   return <div className={classes} />;
 }
