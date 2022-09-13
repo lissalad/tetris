70acaa7ae2cdea17f9273bc4777adae6108423c1
lissalad <elissalaymon@gmail.com>
2022-09-13 13:52:55 -0700

    next block connection

--- src/components/NextBlock.js
+++ src/components/NextBlock.js
@@ -1,8 +1,12 @@
 import GridSquare from "./GridSquare";
+import { useSelector } from 'react-redux'
+import { shapes } from '../utils'
 
 export default function NextBlock(props) {
+  const nextShape = useSelector((state) => state.game.nextShape)
+	const box = shapes[nextShape][0] // Get the first rotation
 
-  const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+  // const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
 	const grid = box.map((rowArray, row) => {
 		return rowArray.map((square, col) => {
 			return <GridSquare key={`${row}${col}`} color={square} />
