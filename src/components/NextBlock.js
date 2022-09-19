dd6bcf26f2ba26e9cf2d5de76c0514d339cc86ca
lissalad <elissalaymon@gmail.com>
2022-09-18 19:30:15 -0700

    rotate shape, shape colors fixed

--- src/components/NextBlock.js
+++ src/components/NextBlock.js
@@ -5,11 +5,14 @@ import { shapes } from '../utils'
 export default function NextBlock(props) {
   const nextShape = useSelector((state) => state.game.nextShape)
 	const box = shapes[nextShape][0] // Get the first rotation
-
+  
   // const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
 	const grid = box.map((rowArray, row) => {
-		return rowArray.map((square, col) => {
-			return <GridSquare key={`${row}${col}`} color={square} />
+    return rowArray.map((square, col) => {
+      if (square === 0) {
+        return <GridSquare key={`${row}${col}`} color="0" />
+      }
+        return <GridSquare key={`${row}${col}`} color={nextShape} />
 		})
 	})
 
--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -1,30 +1,42 @@
-import { defaultState } from '../utils'
+import { defaultState, nextRotation, canMoveTo } from "../utils";
 import {
-  MOVE_RIGHT, MOVE_LEFT, MOVE_DOWN, ROTATE,
-  PAUSE, RESUME, RESTART, GAME_OVER
-} from '../actions'
+  MOVE_RIGHT,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  ROTATE,
+  PAUSE,
+  RESUME,
+  RESTART,
+  GAME_OVER,
+} from "../actions";
 
 const gameReducer = (state = defaultState(), action) => {
-  switch(action.type) {
-    case ROTATE:
-      return state
-    case MOVE_RIGHT:
-      return state
-    case MOVE_LEFT:
-      return state
-    case MOVE_DOWN:
-      return state
-    case RESUME:
-      return state
-    case PAUSE:
-      return state
-    case GAME_OVER:
-      return state
-    case RESTART:
-      return state
-    default:
-      return state
-  }
-}
+  const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state;
 
-export default gameReducer
+  switch (action.type) {
+    case ROTATE:
+      const newRotation = nextRotation(shape, rotation);
+      if (canMoveTo(shape, grid, x, y, newRotation)) {
+        return { ...state, rotation: newRotation };
+      }
+      return state;
+    case MOVE_RIGHT:
+      return state;
+    case MOVE_LEFT:
+      return state;
+    case MOVE_DOWN:
+      return state;
+    case RESUME:
+      return state;
+    case PAUSE:
+      return state;
+    case GAME_OVER:
+      return state;
+    case RESTART:
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default gameReducer;
