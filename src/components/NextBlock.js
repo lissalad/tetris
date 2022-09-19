655e268f50adbdf50ccf8ffcce4f16a715a3d6c4
lissalad <elissalaymon@gmail.com>
2022-09-19 11:34:07 -0700

    move left and right

--- src/components/NextBlock.js
+++ src/components/NextBlock.js
@@ -1,24 +1,20 @@
 import GridSquare from "./GridSquare";
-import { useSelector } from 'react-redux'
-import { shapes } from '../utils'
+import { useSelector } from "react-redux";
+import { shapes } from "../utils";
 
 export default function NextBlock(props) {
-  const nextShape = useSelector((state) => state.game.nextShape)
-	const box = shapes[nextShape][0] // Get the first rotation
-  
+  const nextShape = useSelector((state) => state.game.nextShape);
+  const box = shapes[nextShape][0]; // Get the first rotation
+
   // const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-	const grid = box.map((rowArray, row) => {
+  const grid = box.map((rowArray, row) => {
     return rowArray.map((square, col) => {
       if (square === 0) {
-        return <GridSquare key={`${row}${col}`} color="0" />
+        return <GridSquare key={`${row}${col}`} color="0" />;
       }
-        return <GridSquare key={`${row}${col}`} color={nextShape} />
-		})
-	})
+      return <GridSquare key={`${row}${col}`} color={nextShape} />;
+    });
+  });
 
-  return (
-		<div className="next-block">
-			{grid}
-		</div>
-	)
+  return <div className="next-block">{grid}</div>;
 }
--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -14,15 +14,30 @@ const gameReducer = (state = defaultState(), action) => {
   const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state;
 
   switch (action.type) {
+    
+    // rotate
     case ROTATE:
       const newRotation = nextRotation(shape, rotation);
+      // check if rotation possible
       if (canMoveTo(shape, grid, x, y, newRotation)) {
         return { ...state, rotation: newRotation };
       }
       return state;
+
+    // move right
     case MOVE_RIGHT:
+      // add 1 to x, see if possible
+      if (canMoveTo(shape, grid, x + 1, y, rotation)) {
+        return { ...state, x: x + 1 };
+      }
       return state;
+
+    // move left
     case MOVE_LEFT:
+      // subtract 1 from x, check if possible
+      if (canMoveTo(shape, grid, x - 1, y, rotation)) {
+        return { ...state, x: x - 1 };
+      }
       return state;
     case MOVE_DOWN:
       return state;
